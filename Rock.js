15fbd0e8c1625459baa48e3d707daa505f70c730
SHARODHVARMA <83021312+SHARODHVARMA@users.noreply.github.com>
2021-05-06 20:10:40 +0530

    Add files via upload

---
+++ Rock.js
@@ -0,0 +1,24 @@
+class Rock{
+    constructor(x,y,width,height){
+      var options ={ isStatic: false}
+      
+      this.body =  Bodies.rectangle(x,y,width,height,options);
+      World.add(myWorld,this.body);
+      this.width = width;
+      this.height = height;
+
+      this.image = loadImage('images/rock1.png');
+      World.add(myWorld,this.body);
+    }
+    show()
+    {
+        imageMode(CENTER);
+        var pos = this.body.position;
+        fill('BLACK');
+        stroke("GREY");
+        image(this.image,pos.x,pos.y,this.width, this.height);
+    }
+  
+  
+  }
+  
